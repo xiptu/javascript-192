52c69d48043958eca1493c89dd38d2cee86c195e
giorgi kardava <xiptu102030z@gmail.com>
2025-06-07 11:52:19 +0400

    Add files via upload

---
+++ script - Copy.js
@@ -0,0 +1,28 @@
+let ellement =30;
+
+if(ellement < 50) {
+    console.log("ნაკლებია 50ზე");
+}else if (ellement > 20) {
+    console.log("მეტია 20ზე");
+}else {
+    console.log("შეცდომა");
+}
+
+
+// if პირობა = მარიამს
+
+let name='მარიამი';
+ console.log(name ==='მარიამი'? "true":"false");
+ console.log(name ==='მარი'? "true":"false");
+
+// switch case_ს საშუალებით 
+
+let name1='მარიამი'
+
+switch (name1) {
+    case "მარიამი":
+    console.log("true");
+    break;
+    default:
+        console.log("false");
+}
